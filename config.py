9d0baa30d45941ba7476a4c883bf62c48fb099ae
venkat2109 <venkatramasai@gmail.com>
2020-07-03 12:14:47 -0500

    loading db from MySQL to PostgreSQL

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ DB_DETAILS = {
         'SOURCE_DB': {
             'DB_TYPE': 'mysql',
             'DB_HOST': '34.72.140.44',
-            'DB_NAME': 'retail',
+            'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('SOURCE_DB_USER'),
             'DB_PASS': os.environ.get('SOURCE_DB_PASS'),
 
         },
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': '34.72.140.44',
+            'DB_HOST': '34.72.140.144',
             'DB_NAME': 'retail_dw',
             'DB_USER': os.environ.get('TARGET_DB_USER'),
             'DB_PASS': os.environ.get('TARGET_DB_PASS'),
